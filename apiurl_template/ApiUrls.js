4856b953d97369d57395a6a390285be1ca750e75
jigeorge <jinu_george@bmc.com>
2022-06-23 06:32:24 -0500

    First Commit

---
+++ apiurl_template/ApiUrls.js
@@ -0,0 +1,12 @@
+const baseurl = "https://customurl-di.trybmc.com/django"
+// const baseurl = "http://localhost:8080/django";
+// const baseurl = "http://127.0.0.1:8000/django"
+
+export const API_BHOM_POST = baseurl + "/dataingester/bhom/";
+
+export const API_BHOM_CLOSE = baseurl + "/dataingester/bhom/close/";
+
+export const API_ITSM_POST = baseurl + "/dataingester/itsm/";
+
+export const API_ITSM_CLOSE = baseurl + "/dataingester/itsm/close/"
+
